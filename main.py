75cb208c7d463cf4a63283835629c047d81fd634
Mirren Shah <shahmirren@gmail.com>
2023-11-07 19:16:11 +0000

    decision tree model

--- main.py
+++ main.py
@@ -3,53 +3,60 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn import tree
 from sklearn.tree import DecisionTreeClassifier
-import matplotlib.pyplot as plt
 
 from sklearn.metrics import accuracy_score, classification_report
 
-## Read training dataset
-trainingData = pd.read_csv('assets/fraudTrain.csv')
+def processDB(file):
+    ## Read training dataset
+    data = pd.read_csv(file)
 
-## Clean dataset
-trainingData.dropna(inplace=True)
+    ## Clean dataset
+    data.dropna(inplace=True)
 
-# Label encoding for binary categories
-label_encoder = LabelEncoder()
-trainingData['category'] = label_encoder.fit_transform(trainingData['category'])
+    # Label encoding for binary categories
+    label_encoder = LabelEncoder()
+    data['category'] = label_encoder.fit_transform(data['category'])
 
-# Frequency encoding for high-cardinality categories
-frequency_map = trainingData['merchant'].value_counts().to_dict()
-trainingData['merchant'] = trainingData['merchant'].map(frequency_map)
+    # Frequency encoding for high-cardinality categories
+    frequency_map = data['merchant'].value_counts().to_dict()
+    data['merchant'] = data['merchant'].map(frequency_map)
 
-# Extract date and time components
-trainingData['trans_date_trans_time'] = pd.to_datetime(trainingData['trans_date_trans_time'])
-trainingData['trans_year'] = trainingData['trans_date_trans_time'].dt.year
-trainingData['trans_month'] = trainingData['trans_date_trans_time'].dt.month
-trainingData['trans_day'] = trainingData['trans_date_trans_time'].dt.day
-trainingData['trans_hour'] = trainingData['trans_date_trans_time'].dt.hour
-trainingData['trans_minute'] = trainingData['trans_date_trans_time'].dt.minute
-trainingData['trans_second'] = trainingData['trans_date_trans_time'].dt.second
+    # Extract date and time components
+    data['trans_date_trans_time'] = pd.to_datetime(data['trans_date_trans_time'])
+    data['trans_year'] = data['trans_date_trans_time'].dt.year
+    data['trans_month'] = data['trans_date_trans_time'].dt.month
+    data['trans_day'] = data['trans_date_trans_time'].dt.day
+    data['trans_hour'] = data['trans_date_trans_time'].dt.hour
+    data['trans_minute'] = data['trans_date_trans_time'].dt.minute
+    data['trans_second'] = data['trans_date_trans_time'].dt.second
 
-trainingData.to_csv('assets/preprocessed_fraudTrain.csv', index=False)
+    return data
 
-tempData = pd.read_csv('assets/preprocessed_fraudTrain.csv')
+processedData = processDB('assets/fraudTrain.csv')
 
 # Select features and target variable
-features = ['merchant', 'category', 'amt', 'unix_time', 'merch_lat', 'merch_long', 'trans_year', 'trans_month', 'trans_day', 'trans_hour', 'trans_minute', 'trans_second']
-X = tempData[features]
-y = tempData['is_fraud']
+# features = ['merchant', 'category', 'amt', 'unix_time', 'merch_lat', 'merch_long', 'trans_year', 'trans_month', 'trans_day', 'trans_hour', 'trans_minute', 'trans_second']
+features = ['category', 'amt', 'city_pop']
+X_train = processedData[features]
+y_train = processedData['is_fraud']
 
 # Create a decision tree classifier
 clf = DecisionTreeClassifier(max_depth=10)
 
 # Train the model
-clf.fit(X, y)
+clf.fit(X_train, y_train)
 
 # Evaluate the model (optional)
-# If you want to evaluate the model's performance on the training trainingData, you can use the same code as before, with X and y being the full dataset.
-y_pred = clf.predict(X)
-accuracy = accuracy_score(y, y_pred)
-report = classification_report(y, y_pred)
+# If you want to evaluate the model's performance on the training data, you can use the same code as before, with X and y being the full dataset
+
+testData = processDB('assets/fraudTest.csv')
+
+X_test = testData[features]
+y_test = testData['is_fraud']
+
+y_pred = clf.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+report = classification_report(y_test, y_pred)
 
 print(f"Accuracy: {accuracy:.2f}")
 print(report)
