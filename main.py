50387085912ea0c2d5e422822b5943e8f82a41ea
Mizzalini <86927386+Mizzalini@users.noreply.github.com>
2023-11-28 19:26:07 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
+import os
+import feature_gen
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, classification_report
 
 # Constants
-DATA = 'assets/processedFraudTrain.csv'
 MAX_DEPTH = 10
+
+def generate_new_features() -> str:
+    """
+    If the training data has not been feature engineered, new features are generated.
+    
+    Args:
+        None
+        
+    Returns:
+        None
+    """
+    if not os.path.exists(feature_gen.OUTPUT_FILEPATH):
+        feature_gen.main()
+    return feature_gen.OUTPUT_FILEPATH
 
 def read_and_clean_data(file_path: str) -> pd.DataFrame:
     """
@@ -76,8 +91,9 @@ def main() -> None:
     Returns:
         None
     """
+    training_data = generate_new_features()
     # Process training data
-    processed_data_train = read_and_clean_data('assets/processedFraudTrain.csv')
+    processed_data_train = read_and_clean_data(training_data)
 
     # Select features and target variable for training
     features_train = ['category', 'amt', 'city_pop', 'total_transactions', 'average amount over 30 days', 'maximum amount over 30 days']
@@ -90,21 +106,21 @@ def main() -> None:
     # Printing the importance of each feature
     print(clf.feature_importances_)
 
-    # # Evaluate the classifier on the test set
-    # processed_data_test = read_and_clean_data('assets/fraudTest.csv')
+    # Evaluate the classifier on the test set
+    processed_data_test = read_and_clean_data('assets/fraudTest.csv')
     
-    # # Check if features are present in the test data
-    # features_test = features_train
-    # missing_features = set(features_test) - set(processed_data_test.columns)
+    # Check if features are present in the test data
+    features_test = features_train
+    missing_features = set(features_test) - set(processed_data_test.columns)
     
-    # if missing_features:
-    #     raise ValueError(f"Missing features in test data: {missing_features}")
+    if missing_features:
+        raise ValueError(f"Missing features in test data: {missing_features}")
     
-    # features_test = features_train  # Assuming the same features are used for testing
-    # X_test = processed_data_test[features_test]
-    # y_test = processed_data_test['is_fraud']
+    features_test = features_train  # Assuming the same features are used for testing
+    X_test = processed_data_test[features_test]
+    y_test = processed_data_test['is_fraud']
     
-    # eval_classifier(clf, X_test, y_test)
+    eval_classifier(clf, X_test, y_test)
 
 if __name__ == '__main__':
     main()
